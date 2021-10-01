3f0227d74e4820e94b2209a04fee623b674e84b2
vhadlock <vhadlock@users.noreply.github.com>
2021-10-01 10:45:05 -0400

    grayscale cleanup/comments

--- grayscale.py
+++ grayscale.py
@@ -1,35 +1,48 @@
 # Python 3.8.2
 #
 # Converts all images to greyscale
-#
+# Computer Vision, Project 1
 # Stav Rones, Vanessa Hadlock
 
 from genericpath import isdir
-from PIL import Image 
+from PIL import Image
 import os
 
-# Converts an image to greyscale
+
+#########################################################################################################
+# Function that takes in the image from the old color directory, converts an image to greyscale, and
+# returns it to the new greyscale directory
+# params:	oldPath, newPath
+# returns:	none
+#########################################################################################################
 def convert2grey(oldPath, newPath):
     Image.open(oldPath).convert('L').save(newPath)
 
-# Define image locations
+
+# Define image locations for Office & ReChair images, both greyscale and color
 office_color_dir = './images/Office/Color'
 chair_color_dir = './images/RedChair/Color'
 office_grey_dir = './images/Office/Greyscale'
 chair_grey_dir = './images/Office/Greyscale'
 
-# Empty existing Grayscale directories
+# Empty the existing Grayscale directories
 print('Emptying existing greyscale image directories...')
 
-if (os.path.isdir(office_grey_dir)):
+# If the Office greyscale directory exists, delete every image in the directory
+if os.path.isdir(office_grey_dir):
     for g_image in os.listdir(office_grey_dir):
         os.remove(f'{office_grey_dir}/{g_image}')
+
+# If the directory doesn't exist, make the Office greyscale directory
 else:
     os.mkdir(office_grey_dir)
-    
-if (os.path.isdir(chair_grey_dir)):
+
+# If the RedChair greyscale directory exists, delete every image in the directory
+if os.path.isdir(chair_grey_dir):
     for g_image in os.listdir(chair_grey_dir):
         os.remove(f'{chair_grey_dir}/{g_image}')
+
+# If the directory doesn't exist, make the RedChair greyscale directory
 else:
     os.mkdir(chair_grey_dir)
 
@@ -40,7 +53,7 @@ print('Converting all Office images to greyscale...')
 for color_image in os.listdir(office_color_dir):
     oldPath = f'{office_color_dir}/{color_image}'
     newPath = f'{office_grey_dir}/g_{color_image}'
-    convert2grey(oldPath,newPath)
+    convert2grey(oldPath, newPath)
 
 # Convert chair images to greyscale
 print('Converting all Chair images to greyscale...')
@@ -48,4 +61,4 @@ print('Converting all Chair images to greyscale...')
 for color_image in os.listdir(chair_color_dir):
     oldPath = f'{chair_color_dir}/{color_image}'
     newPath = f'{chair_grey_dir}/g_{color_image}'
-    convert2grey(oldPath,newPath)
+    convert2grey(oldPath, newPath)
